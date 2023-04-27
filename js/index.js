6e2d983b3c98bd5a18dd5eef96bece78b6a0b466
edward <edward_gio@hotmail.com>
2023-04-27 15:36:40 -0500

    Arrow functions added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
 const Ejecutar = () => {
-  alert("JS Ejecutado");
+  // alert("JS Ejecutado");
+  // to execute scraping,
+  console.log(document.querySelectorAll(".item-0-2-764"));
+
+  // o
+
+  console.log(document.querySelectorAll("[class*=item-]"));
+
+  // o
+
+  console.log(document.querySelectorAll("[class*=carousel] > [class*=item-]"));
+
+  // ARROW FUNCTIONS *
+
+  // Transforms an object and returns it in a different structure
+
+  let sugerencias = document.querySelectorAll(
+    "[class*=carousel] > [class*=item-]"
+  );
+
+  // with spreat method, make it an array of elements, and be able to use arrow functions
+  sugerencias = [...sugerencias];
+
+  // retreive only the text and not the html element
+  let listaTextoSugerencias = sugerencias.map((e) => e.innerText);
+  console.log(listaTextoSugerencias);
+
+  const textoComprador = listaTextoSugerencias.filter((e) =>
+    e.includes("Ejecutivo")
+  );
+
+  console.log(textoComprador.length);
+  console.log(textoComprador);
+
+  const textPrimerComprador = listaTextoSugerencias.find((e) =>
+    e.includes("Ejecutivo")
+  );
+  console.log(textPrimerComprador);
+
+  const textPrimerCompradorIndex = listaTextoSugerencias.findIndex((e) => {
+    e = textPrimerComprador;
+  });
+  console.log(textPrimerCompradorIndex);
+
+  console.log(listaTextoSugerencias.fill("hola", 4, 6));
 };
