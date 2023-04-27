42c4627bcf696f239352d74eb238c3b29f6152de
Rubén Mantilla <rvben94@gmail.com>
2023-04-26 20:48:57 -0500

    Agregando clasificacion por salario

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const buttonElement = document.getElementById("procesar");
+// Obteniendo datos de la página
+const datos = document.querySelectorAll(
+  "div[class*=carouselCont] > div[class*=carouselWrap] > div[class*=row] > div[class*=item] > div[class*=similarJob]"
+);
+
+// Clasificando datos
+const paises = ["PERU", "CHILE", "COLOMBIA", "MEXICO"];
+const salarios = [
+  "Salario no mostrado.",
+  "$1,000 - $2,000",
+  "$2,000 - $3,000",
+  "$4,000 - $6,000",
+  "$9,000 - $11,000",
+  "$12,000 - $13,000",
+];
 
 buttonElement.addEventListener("click", () => {
-  // Obteniendo datos de la página
-  let datos = document.querySelectorAll(
-    "div[class*=carouselCont] > div[class*=carouselWrap] > div[class*=row] > div[class*=item] > div[class*=similarJob]"
-  );
-
   // Convirtiendo datos a un array
   let datosArray = [...datos];
-  let paises = ["PERU", "CHILE", "COLOMBIA", "MEXICO"];
 
-  // Mostrando datos en un objeto
+  // Mostrando datos en un array de objetos por país
   let porPaises = paises.map((pais) => {
     let inToCountry = datosArray.filter((el) =>
       el.querySelector("a").innerText.includes(pais)
@@ -28,5 +37,28 @@ buttonElement.addEventListener("click", () => {
     };
   });
 
+  // Mostrando datos en un array de objetos por salario
+  let porSalarios = salarios.map((salario) => {
+    let inToSalario = datosArray.filter((el) =>
+      el
+        .querySelector("p[id*=vacante_similar_salario-]")
+        .innerText.includes(salario)
+    );
+
+    return {
+      salario: salario,
+      trabajos: inToSalario.map((el) => {
+        return {
+          title: el.querySelector("a").innerText,
+          pais: el.querySelector("a").innerText.split(" ").pop(),
+          rol: el.querySelector("a").innerText.split(" ")[0],
+        };
+      }),
+    };
+  });
+
+  console.group("Trabajos por país");
   console.log(porPaises);
+  console.log(porSalarios);
+  console.groupEnd();
 });
