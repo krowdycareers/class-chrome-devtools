fc85141d7a2f65c2bfd9d9f8d20490873f43c240
Rubén Mantilla <rvben94@gmail.com>
2023-04-27 19:21:10 -0500

    Renombrando Variables

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const buttonElement = document.getElementById("procesar");
 // Obteniendo datos de la página
-const datos = document.querySelectorAll(
+const listJobs = document.querySelectorAll(
   "div[class*=carouselCont] > div[class*=carouselWrap] > div[class*=row] > div[class*=item] > div[class*=similarJob]"
 );
 
 // Clasificando datos
-const paises = ["PERU", "CHILE", "COLOMBIA", "MEXICO"];
-const salarios = [
+const countrys = ["PERU", "CHILE", "COLOMBIA", "MEXICO"];
+const salaries = [
   "Salario no mostrado.",
   "$1,000 - $2,000",
   "$2,000 - $3,000",
@@ -17,17 +17,17 @@ const salarios = [
 
 buttonElement.addEventListener("click", () => {
   // Convirtiendo datos a un array
-  let datosArray = [...datos];
+  let listJobsArray = [...listJobs];
 
   // Mostrando datos en un array de objetos por país
-  let porPaises = paises.map((pais) => {
-    let inToCountry = datosArray.filter((el) =>
+  let orderForCountry = countrys.map((pais) => {
+    let listJobsForCountry = listJobsArray.filter((el) =>
       el.querySelector("a").innerText.includes(pais)
     );
 
     return {
       pais: pais,
-      trabajos: inToCountry.map((el) => {
+      trabajos: listJobsForCountry.map((el) => {
         return {
           title: el.querySelector("a").innerText,
           salario: el.querySelector("p[id*=vacante_similar_salario]").innerText,
@@ -38,8 +38,8 @@ buttonElement.addEventListener("click", () => {
   });
 
   // Mostrando datos en un array de objetos por salario
-  let porSalarios = salarios.map((salario) => {
-    let inToSalario = datosArray.filter((el) =>
+  let orderForSalary = salaries.map((salario) => {
+    let listJobsForSalary = listJobsArray.filter((el) =>
       el
         .querySelector("p[id*=vacante_similar_salario-]")
         .innerText.includes(salario)
@@ -47,7 +47,7 @@ buttonElement.addEventListener("click", () => {
 
     return {
       salario: salario,
-      trabajos: inToSalario.map((el) => {
+      trabajos: listJobsForSalary.map((el) => {
         return {
           title: el.querySelector("a").innerText,
           pais: el.querySelector("a").innerText.split(" ").pop(),
@@ -58,9 +58,9 @@ buttonElement.addEventListener("click", () => {
   });
 
   console.group("Trabajos por país");
-  console.log(porPaises);
+  console.log(orderForCountry);
   console.groupEnd();
   console.group("Trabajos por Salarios");
-  console.log(porSalarios);
+  console.log(orderForSalary);
   console.groupEnd();
 });
