3a7daeba3e9459e46d137025eb5fa5f5239c71ad
Cristian Mendoza <criszmendoza212@gmail.com>
2023-04-27 18:19:24 -0600

    Corrección nombres de variables

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const NODOS = [...document.querySelectorAll('div[class^="similarJob"]')];
-const publicaciones = { mexico: [], peru: [], colombia: [], chile: [] };
+const JOBS_NODOS = [...document.querySelectorAll('div[class^="similarJob"]')];
+let publications = { mexico: [], peru: [], colombia: [], chile: [] };
 
-const jobs = NODOS.map((nodo) => ({
+const jobs = JOBS_NODOS.map((nodo) => ({
   titulo: nodo.querySelector('a > p').innerText,
   salario: nodo.querySelector('p[id*="salario"]').innerText,
 }));
 
 jobs.forEach((job) => {
-  if (job.titulo.includes('MEXICO')) publicaciones['mexico'].push(job);
-  if (job.titulo.includes('PERU')) publicaciones['peru'].push(job);
-  if (job.titulo.includes('COLOMBIA')) publicaciones['colombia'].push(job);
-  if (job.titulo.includes('CHILE')) publicaciones['chile'].push(job);
+  if (job.titulo.includes('MEXICO')) publications['mexico'].push(job);
+  if (job.titulo.includes('PERU')) publications['peru'].push(job);
+  if (job.titulo.includes('COLOMBIA')) publications['colombia'].push(job);
+  if (job.titulo.includes('CHILE')) publications['chile'].push(job);
 });
 
-const json = JSON.stringify(publicaciones);
+const json = JSON.stringify(publications);
 
 console.log(json);
