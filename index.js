bb101592400c4eb0f0bdbe28818f73b131255601
paotovar <ptovargavilan@gmail.com>
2023-04-30 20:55:07 -0500

    adding changes

--- index.js
+++ index.js
@@ -1,63 +1,47 @@
-const buttonElement  = document.getElementById("procesar")
-const datos = document.querySelectorAll("div[class*=similarJob]")
-const datosArray = [...datos]
-const paises = ["PERU", "MEXICO", "CHILE", "COLOMBIA"]
+const buttonElement = document.getElementById("process");
+const similarJobElements = document.querySelectorAll("div[class*=similarJob]");
+const countries = ["PERU", "MEXICO", "CHILE", "COLOMBIA"];
 
-const arreglo = datosArray.map(el => { return { Puesto: el.querySelector('p').innerText, Salario: el.querySelector('[id*=vacante_similar_salario]').innerText } })
-console.log(arreglo)
-const arrayPais = {}
-const arraySalario = {}
+const arrangements = Array.from(similarJobElements).map((el) => ({
+  position: el.querySelector("p").innerText,
+  salary: el.querySelector("[id*=vacante_similar_salario]").innerText,
+}));
+console.log(arrangements);
 
-buttonElement.addEventListener('click', () => {
-    alert("Países y Salarios");
-    
-    const paisesIndex = {};
-    for (let i = 0; i < paises.length; i++) {
-      paisesIndex[paises[i]] = true;
-    }
-    
-    const arrayPais = {};
-    const arraySalario = {};
-    for (let i = 0; i < arreglo.length; i++) {
-      const element = arreglo[i];
-      let encontrado = false;
-      for (let pais in paisesIndex) {
-        if (element.Puesto.includes(pais)) { 
-          if (!arrayPais[pais]) {
-            arrayPais[pais] = []; 
-          }
-          arrayPais[pais].push(element);
-          encontrado = true;
-          break;
-        }
-      }
-      if (!encontrado) {
-        if (!arrayPais.Otros) {
-          arrayPais.Otros = [];
-        }
-        arrayPais.Otros.push(element);
-      }
-      
-      const salario = element.Salario;
-      if (!arraySalario.hasOwnProperty(salario)) {
-        arraySalario[salario] = [];
-      }
-      arraySalario[salario].push(element);
-    }
-  
-    for (let pais in arrayPais) {
-      if (arrayPais.hasOwnProperty(pais)) {
-        arrayPais[pais].sort((a, b) => {
-          if (a.Salario === "Salario no mostrado.") return 1;
-          if (b.Salario === "Salario no mostrado.") return -1;
-          const asalario = parseInt(a.Salario.replace(/[^0-9.-]+/g, ""));
-          const bsalario = parseInt(b.Salario.replace(/[^0-9.-]+/g, ""));
-          return bsalario - asalario;
-        });
-      }
-    }
-    
-    console.log(arrayPais);
-    console.log(arraySalario);
+const groupByCountry = (arr) =>
+  arr.reduce((acc, element) => {
+    const found = countries.find((country) =>
+      element.position.includes(country)
+    );
+    const countryKey = found || "Others";
+    acc[countryKey] = acc[countryKey] || [];
+    acc[countryKey].push(element);
+    return acc;
+  }, {});
+
+const sortArrangementsBySalary = (arr) =>
+  arr.sort((a, b) => {
+    if (a.salary === "Salary not shown.") return 1;
+    if (b.salary === "Salary not shown.") return -1;
+    const aSalary = parseInt(a.salary.replace(/[^0-9.-]+/g, ""));
+    const bSalary = parseInt(b.salary.replace(/[^0-9.-]+/g, ""));
+    return bSalary - aSalary;
   });
-  
+
+buttonElement.addEventListener("click", () => {
+  alert("Countries and Salaries");
+
+  const countryGroups = groupByCountry(arrangements);
+  for (let country in countryGroups) {
+    sortArrangementsBySalary(countryGroups[country]);
+  }
+  console.log(countryGroups);
+
+  const salaryGroups = arrangements.reduce((acc, element) => {
+    const salary = element.salary;
+    acc[salary] = acc[salary] || [];
+    acc[salary].push(element);
+    return acc;
+  }, {});
+  console.log(salaryGroups);
+});
