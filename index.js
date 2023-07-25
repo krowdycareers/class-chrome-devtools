55e9e367af7cd0d83dd11cacf62f1a4af198ce81
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2023-07-24 22:30:13 -0500

    [add] group countries and salary range

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const btnProcesar = document.getElementById("btn-procesar");
 
+const countries = ["PERU", "CHILE", "MEXICO", "COLOMBIA"];
+
 btnProcesar.addEventListener("click", () => {
   const elementCards = document.querySelectorAll(".similarJob-0-2-766");
   let arrayCards = [...elementCards];
-  const arrayJobs = arrayCards.map((el) => {
+  let arrayJobs = arrayCards.map((el) => {
     const elementTitle = el.querySelector('a[id|="vacante_similar"]');
     const elementSalary = el.querySelector("p[id|=vacante_similar_salario]");
+    const title = elementTitle?.innerText;
+    const country = countries.find((el) => title.includes(el));
+
     return {
-      title: elementTitle?.innerText,
+      title,
       url: elementTitle?.href,
       rangeSalary: elementSalary.innerText,
+      country,
     };
   });
 
-  console.log(arrayJobs);
+  let group = [];
+
+  arrayJobs.forEach((el) => {
+    let groupContrySalary = group.find(
+      (elg) => el.country == elg.country && elg.rangeSalary == el.rangeSalary
+    );
+
+    if (!groupContrySalary)
+      group.push({
+        country: el.country,
+        rangeSalary: el.rangeSalary,
+        jobs: [el],
+      });
+    else groupContrySalary.jobs.push(el);
+  });
+
+  console.log(group);
 });
